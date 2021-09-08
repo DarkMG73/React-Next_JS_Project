1dc7dda28a395e8c8e8f728d82a6445afcccde1e
DarkMG73 <levelthreeemail@gmail.com>
2021-09-08 00:35:33 -0400

    Update

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -11,4 +11,40 @@ function MeetupDetail(props) {
   );
 }
 
+export async function getStaticPaths() {
+  // fetch data for for each meetup.
+
+  return {
+    fallback: true, // false generate 404, true means it will try to generate dynamically if not defined below.
+    paths: [
+      {
+        params: {
+          meetupId: meetupId1,
+        },
+        params: {
+          meetupId: meetupId1,
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup.
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
+        id: meetupId,
+        title: "A First Meetup",
+        address: "Some Address",
+        description: "This is first meetup.",
+      },
+    },
+  };
+}
 export default MeetupDetail;
